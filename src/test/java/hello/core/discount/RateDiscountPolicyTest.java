bc3f9d56bf17bfa2b5d95ccc7940a55bdf6f7fa2
unknown <me123alsl@gmail.com>
2022-08-02 13:15:43 +0900

    Spring ãBoot Exam Save

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,43 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("BASIC 할인 안함")
+    void vip_o() {
+//        given
+        Member member1 = new Member(1L, "memberA", Grade.VIP);
+
+//        when
+        int discount = discountPolicy.discount(member1, 10000);
+
+//        then
+        assertThat(discount).isEqualTo(1000);
+
+    }
+
+    @Test
+    @DisplayName("VIP 10% 할인")
+    void vip_x() {
+//        given
+        Member member2 = new Member(2L, "memberB", Grade.BASIC);
+
+//        when
+        int discount = discountPolicy.discount(member2, 10000);
+
+//        then
+        assertThat(discount).isEqualTo(1000);
+
+    }
+}
---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,39 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // 사용자 A 가 만원 주문
+        statefulService1.order("userA", 10000);
+        // 사용자 B 가 2만원 주문
+        statefulService1.order("userB", 20000);
+
+        // 사용자 A 가 주문금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+
+}
