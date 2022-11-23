a8014d7642cfd8b0a83b2d342ee637425533b98e
JinseokSong <me123alsl@gmail.com>
2022-11-24 00:50:35 +0900

    완료

--- src/main/java/hello/core/HelloLombok.java
+++ src/main/java/hello/core/HelloLombok.java
@@ -14,11 +14,11 @@ public class HelloLombok {
     private int age;
 
     public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setName("Spring");
-
-        String name = helloLombok.getName();
-        System.out.println("name = " + name);
+//        HelloLombok helloLombok = new HelloLombok();
+//        helloLombok.setName("Spring");
+//
+//        String name = helloLombok.getName();
+//        System.out.println("name = " + name);
 
     }
 }
---
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -0,0 +1,24 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+
+    // scope only
+//    private  final ObjectProvider<MyLogger> myLoggerProvider;
+
+    // proxy
+    private  final MyLogger myLogger;
+
+    public void logic(String id){
+        // scope only
+//        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id= "+ id);
+    }
+}
